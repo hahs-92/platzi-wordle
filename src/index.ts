fc4f95ba8e0e71180c2c81256a0b41deed7b3d0b
hahs <alexandre_2420@outlook.com>
2022-09-27 11:14:55 -0500

    create our first observable

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Observable, Subscriber } from "rxjs";
+
+const observableAlpha$ = new Observable((subscriber) => {
+  subscriber.next(1);
+
+  subscriber.next(2);
+  subscriber.next(3);
+  //subscriber.complete();
+  //   __________deja de emitir valores_____________
+  subscriber.next(4);
+  //   ____________generando error__________________
+  throw "ERROR GENERADO";
+  subscriber.next("hola");
+});
+
+const observer = {
+  next: (value: number | unknown) => {
+    console.log(value);
+  },
+  complete: () => {
+    console.log("Observable completed");
+  },
+  error: (err: string) => {
+    console.error("Error: ", err);
+  },
+};
+
+observableAlpha$.subscribe(observer);
